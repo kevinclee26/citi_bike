0ecd433907c8a6153cb598e922092ca41cd5534d
kevinclee26 <kevin.c.lee26@gmail.com>
2021-04-10 22:30:31 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # citi_bike
+
+This is a project to map out the rentral bike stations in NYC
---
+++ assets/js/config.js
@@ -0,0 +1,1 @@
+const API_KEY="pk.eyJ1Ijoia2V2aW5jbGVlMjYiLCJhIjoiY2tmdTF4dGpoMGpnOTJxcWZ2bTRkZHB4ZyJ9.uN_ZmcdWxJmpwLGCKREFOg"
